b03f57fbaf89ea4a11c1a57ebfed9a5b0ed9836d
Umarov Timur <jojinet1@gmail.com>
2022-12-05 12:18:09 +0300

    I think i'm done with Hello world file

--- Hello world.py
+++ Hello world.py
@@ -1,17 +1,24 @@
 from jinja2 import Template
 
-name = "Timur"
-age = 20
+class HumanBieng:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+timur = HumanBieng("Timur", 20)
 
-templ = Template("Hi {{ n }}")
-message = templ.render(n = name)
-templ2 = Template("You are {{ a }} years old, damn you are child")
-ageMessage = templ2.render(a = age)
+template = Template("Hi, my name is {{ t.name }} and i'm {{ t.age }} years old")
+message = template.render(t = timur)
 
-print(message, ageMessage, sep="\n")
+print(message)
 
+#name = "Timur"
+#age = 20
 
+#templ = Template("Hi {{ n }}")
+#message = templ.render(n = name)
+#templ2 = Template("You are {{ a }} years old, damn you are child")
+#ageMessage = templ2.render(a = age)
 
 #message2 = f"Привет {name}"
-
 #print(message2)
