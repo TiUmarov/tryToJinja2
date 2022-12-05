1a98e7d1e6c577ec0d14ef09c7f52b88d7627393
Umarov Timur <jojinet1@gmail.com>
2022-12-05 12:02:45 +0300

    New slovar'

--- Hello world.py
+++ Hello world.py
@@ -1,8 +1,17 @@
 from jinja2 import Template
 
 name = "Timur"
+age = 20
 
-templ = Template("Hi {{ name }}")
-message = templ.render(name = name)
+templ = Template("Hi {{ n }}")
+message = templ.render(n = name)
+templ2 = Template("You are {{ a }} years old, damn you are child")
+ageMessage = templ2.render(a = age)
 
-print(message)
+print(message, ageMessage, sep="\n")
+
+
+
+#message2 = f"Привет {name}"
+
+#print(message2)
